786b935146513f95e842580e49efbc29260ec221
Daniel Dykiel <dana.dykiel@gmail.com>
2019-11-01 11:51:55 -0400

    Wrote and implemented Maze interface
    Slight edit: getDirections() now returns List, not ArrayList.

--- packageFiles/Maze.java
+++ packageFiles/Maze.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
+import java.util.List;
 
 public interface Maze
 {
     int getMaxX();
     int getMaxY();
-    ArrayList<DirType> getDirections(int y, int x);
+    List<DirType> getDirections(int y, int x);
     Integer[] startPoint();
     Integer[] endPoint();
     Integer[] playerPos();
